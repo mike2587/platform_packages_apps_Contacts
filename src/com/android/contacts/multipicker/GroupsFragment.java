0c21238b7ee0dc108b751eb3e02404877aee1395
Steve Kondik <steve@cyngn.com>
2016-10-17 21:57:05 -0700

    Merge tag 'LA.UM.5.5.r1-00900-8x96.0' of git://codeaurora.org/platform/packages/apps/Contacts into aosp-7.1
    "LA.UM.5.5.r1-00900-8x96.0"

--- src/com/android/contacts/multipicker/GroupsFragment.java
+++ src/com/android/contacts/multipicker/GroupsFragment.java
@@ -225,12 +225,21 @@ public class GroupsFragment extends ExpandableListFragment implements OnGroupCli
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        fillAllContactsCursorMap();
+    }
+
+    @Override
     public void onDestroy() {
         mAdapter.getQueryHandler().removeCallbacksAndMessages(QUERY_TOKEN);
 
         if (mAdapter.getCursor() != null) {
             mAdapter.getCursor().close();
         }
+
+        if(allContactsInGroups!=null)
+            allContactsInGroups.close();
 
         if (mAllContactsCurosrMap != null) {
             mAllContactsCurosrMap.clear();
@@ -780,7 +789,10 @@ public class GroupsFragment extends ExpandableListFragment implements OnGroupCli
         @Override
         protected Cursor getChildrenCursor(Cursor groupCursor) {
             long groupId = groupCursor.getLong(GROUP_ID);
-            return getContactsDetailCursor(groupId);
+            Cursor c = getContactsDetailCursor(groupId);
+            if (c != null)
+                getActivity().startManagingCursor(c);
+            return c;
         }
 
         @Override
@@ -791,6 +803,7 @@ public class GroupsFragment extends ExpandableListFragment implements OnGroupCli
                 super.changeCursor(cursor);
             }
         }
+    }
 
         /**
          * all contacts cursor fill to map
@@ -798,6 +811,8 @@ public class GroupsFragment extends ExpandableListFragment implements OnGroupCli
         private void fillAllContactsCursorMap() {
             mAllContactsCurosrMap.clear();
             Cursor cursor = null;
+            if (mGroupsCursor == null)
+                return;
             for (int groupPosition = 0; groupPosition < mGroupsCursor.getCount(); groupPosition++) {
                 mGroupsCursor.moveToPosition(groupPosition);
                 long groupCacheId = mGroupsCursor.getLong(GROUP_ID);
@@ -820,7 +835,6 @@ public class GroupsFragment extends ExpandableListFragment implements OnGroupCli
             if (cursor != null) {
                 cursor.close();
             }
-        }
     }
 
     @Override
